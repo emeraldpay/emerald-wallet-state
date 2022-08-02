fed53a4f2ce8897af9af5ea7840307e05f710ac5
Igor Artamonov <igor@artamonov.ru>
2022-08-02 00:07:31 -0400

    problem: no standard location for the state db

--- src/storage.rs
+++ src/storage.rs
@@ -1,5 +1,42 @@
+use std::env;
+use std::path::PathBuf;
+
 pub mod sled_access;
 mod indexing;
 pub mod transaction_store;
 pub mod adressbook_store;
 pub mod xpubpos_store;
+
+/// Default path (*nix)
+#[cfg(all(
+unix,
+not(target_os = "macos"),
+not(target_os = "ios"),
+not(target_os = "android")
+))]
+pub fn default_path() -> PathBuf {
+    let mut config_dir = env::home_dir().expect("Expect path to home dir");
+    config_dir.push(".emerald");
+    config_dir.push("state");
+    config_dir
+}
+
+/// Default path (Mac OS X)
+#[cfg(target_os = "macos")]
+pub fn default_path() -> PathBuf {
+    let mut config_dir = env::home_dir().expect("Expect path to home dir");
+    config_dir.push("Library");
+    config_dir.push("Emerald");
+    config_dir.push("state");
+    config_dir
+}
+
+/// Default path (Windows OS)
+#[cfg(target_os = "windows")]
+pub fn default_path() -> PathBuf {
+    let app_data_var = env::var("APPDATA").expect("Expect 'APPDATA' environment variable");
+    let mut config_dir = PathBuf::from(app_data_var);
+    config_dir.push(".emerald");
+    config_dir.push("state");
+    config_dir
+}
--- src/storage/sled_access.rs
+++ src/storage/sled_access.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use sled::{Db};
 use crate::errors::StateError;
 use crate::storage::adressbook_store::AddressBookAccess;
+use crate::storage::default_path;
 use crate::storage::transaction_store::{TransactionsAccess};
 use crate::storage::xpubpos_store::XPubPositionAccess;
 
@@ -12,7 +13,13 @@ pub struct SledStorage {
 
 /// Sled backed storage
 impl SledStorage {
-    /// Open Sled DB at the specified path
+
+    /// Open DB at the default path
+    pub fn open_default() -> Result<SledStorage, StateError> {
+        SledStorage::open(default_path())
+    }
+
+    /// Open DB at the specified path
     pub fn open(path: PathBuf) -> Result<SledStorage, StateError> {
         let db = sled::open(path)?;
         Ok(SledStorage {
